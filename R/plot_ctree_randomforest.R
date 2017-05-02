993f69aa7682b2e0b783c5950aef29ae3ea5f2cd
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-02 12:48:41 +0200

    delete unnecessary files

--- R/plot_ctree_randomforest.R
+++
@@ -1,6 +0,0 @@
-# this file creates figures for classification trees and random forest
-library("mlr")
-
-# classification tree
-tree = train(learner = "classif.ctree", task = bc.task)
-tree$learner.model
