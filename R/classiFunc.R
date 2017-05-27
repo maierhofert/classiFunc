16ea3e10bbbe2911460cf0530dddaf909495431e
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-27 22:38:33 +0200

    update docu

--- R/classiFunc.R
+++ R/classiFunc.R
@@ -5,16 +5,13 @@
 #' \code{\link{classiKnn}}, a k nearest neighbor estimator for functional data,
 #' and \code{\link{classiKernel}}, a kernel estimator
 #' for functional data. The package uses efficiently implemented semimetrics to
-#' create the distance matrix of the observations in the function
+#' create the distance matrix of the functional observations in the function
 #' \code{\link{computeDistMat}}.
 #' Currently supported distance measures are all \code{methods} implemented in
 #' \code{\link[proxy]{dist}}
-#' and all semimetrics suggested in
-#' Fuchs etal. 2015, Nearest neighbor ensembles for functional data with
-#' interpretable feature selection,
-#' (\url{http://www.sciencedirect.com/science/article/pii/S0169743915001100})
-#' Additionally, all (semi-)metrics can be used on an arbitrary order of
-#' derivation.
+#' and all semimetrics suggested in Fuchs et al. (2015).
+#' Additionally, all (semi-)metrics can be used on a derivative of arbitrary
+#' order of the functional observations.
 #' This is a new package, please report all bugs and issues at
 #' \url{https://github.com/maierhofert/classiFunc}.
 #'
@@ -22,6 +19,14 @@
 #' @importFrom fda Data2fd deriv.fd eval.fd
 #' @importFrom fdasrvf elastic.distance f_to_srvf
 #' @import dtw
+#'
+#' @references
+#' Fuchs, K., J. Gertheiss, and G. Tutz (2015):
+#' Nearest neighbor ensembles for functional data with interpretable feature selection.
+#' Chemometrics and Intelligent Laboratory Systems 146, 186 - 197.
+#'
+#' @author
+#' Thomas Maierhofer
 #'
 #' @rdname classiFunc
 #' @name classiFunc
