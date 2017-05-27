74d294ef2c4c71656695b98d5f964f80ea81d2a1
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-27 22:39:10 +0200

    update docu

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@
 #' important topic in anthropology. The classes are based on shape distinctions
 #' such as the presence and location of a notch in the arrow. The problem in the
 #' repository is a length normalised version of that used in Ye09shapelets.
-#' The three classes are called "Avonlea", "Clovis" and "Mix"."
+#' The three classes are called "Avonlea", "Clovis" and "Mix".
 #'
 #' @section Format A data frame with 100 rows (=observations) and 84 variables:
 #' \describe{
@@ -21,10 +21,10 @@
 
 #' Phonetic Time Series.
 #'
-#' A dataset containing the audio files of english words.
+#' A data set containing the audio files of english words.
 #'
-#' This data set is a subsample of the data used in the paper Dual-domain
-#' Hierarchical Classification of Phonetic Time Series. Each series is extracted
+#' This data set is a subsample of the data used in Hamooni and Mueen (2014).
+#' Each series is extracted
 #' from the segmented audio collected from Google Translate, oxforddictionaries.com
 #' and the Merrriam-Webster online dictionary. Each of these sources have
 #' different features. Audio files collected from Google translate, Oxford, and
@@ -40,5 +40,9 @@
 #'   \item{col 1:64}{one functional observation.}
 #'   \item{target}{encoding the word of the functional observation}
 #' }
+#' @references
+#' Hamooni, Hossein, and Mueen, Abdullah.
+#' "Dual-domain hierarchical classification of phonetic time series."
+#' Data Mining (ICDM), 2014 IEEE International Conference on. IEEE, 2014.
 #' @source \url{http://timeseriesclassification.com/description.php?Dataset=Phoneme}
 "Phoneme"
